9513018820c9c400fa67a4d9b0fe4b11a4dc27d7
nsoevik <njordsoevik@gmail.com>
2022-03-27 19:59:30 -0400

    reformat code

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -2,29 +2,29 @@ import React from "react";
 
 const TuitStats = ({tuit, likeTuit, dislikeTuit}) => {
     return (
-      <div className="row mt-2">
-        <div className="col">
-          <i className="far fa-message me-1"></i>
-          {tuit.stats && tuit.stats.replies}
-        </div>
-        <div className="col">
-          <i className="far fa-retweet me-1"></i>
-          {tuit.stats && tuit.stats.retuits}
-        </div>
-        <div className="col">
+        <div className="row mt-2">
+            <div className="col">
+                <i className="far fa-message me-1"></i>
+                {tuit.stats && tuit.stats.replies}
+            </div>
+            <div className="col">
+                <i className="far fa-retweet me-1"></i>
+                {tuit.stats && tuit.stats.retuits}
+            </div>
+            <div className="col">
           <span onClick={() => likeTuit(tuit)}>
               {
-                tuit.stats && tuit.stats.likes > 0 &&
+                  tuit.stats && tuit.stats.likes > 0 &&
                   <i className="fa-solid fa-thumbs-up" style={{color: 'green'}}></i>
               }
               {
-                tuit.stats && tuit.stats.likes <= 0 &&
+                  tuit.stats && tuit.stats.likes <= 0 &&
                   <i className="fa-solid fa-thumbs-up"></i>
               }
-            {tuit.stats && tuit.stats.likes}
+              {tuit.stats && tuit.stats.likes}
           </span>
-        </div>
-          <div className="col">
+            </div>
+            <div className="col">
           <span onClick={() => dislikeTuit(tuit)}>
               {
                   tuit.stats && tuit.stats.dislikes > 0 &&
@@ -36,11 +36,11 @@ const TuitStats = ({tuit, likeTuit, dislikeTuit}) => {
               }
               {tuit.stats && tuit.stats.dislikes}
           </span>
-          </div>
-        <div className="col">
-          <i className="far fa-inbox-out"></i>
+            </div>
+            <div className="col">
+                <i className="far fa-inbox-out"></i>
+            </div>
         </div>
-      </div>
     );
 }
 export default TuitStats;

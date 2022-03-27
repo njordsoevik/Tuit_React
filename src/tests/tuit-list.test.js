6d6116b2260d1554f352f47869cd2c4b5ec07e0c
nsoevik <njordsoevik@gmail.com>
2022-03-27 12:05:21 -0400

    init

--- src/tests/tuit-list.test.js
+++ src/tests/tuit-list.test.js
@@ -1,52 +1,27 @@
-
-import Tuits from "../components/tuits";
+import {Tuits} from "../components/tuits";
 import {screen, render} from "@testing-library/react";
 import {HashRouter} from "react-router-dom";
 import {findAllTuits} from "../services/tuits-service";
 import axios from "axios";
 
-//jest.mock('axios');
+jest.mock('axios');
 
 const MOCKED_USERS = [
-    {username: 'ellen_ripley', password: 'lv426', email: 'repley@weyland.com', _id: "123"},
-    {username: 'sarah_conor', password: 'illbeback', email: 'sarah@bigjeff.com', _id: "234"},
-]
+  "alice", "bob", "charlie"
+];
+
 const MOCKED_TUITS = [
-    {tuit: 'ellen\'s tuit', postedBy: MOCKED_USERS[0], _id: "223"},
-    {tuit: 'sarah\'s tuit', postedBy: MOCKED_USERS[1], _id: "323"}
+  "alice's tuit", "bob's tuit", "charlie's tuit"
 ];
 
 test('tuit list renders static tuit array', () => {
-  render(
-      <HashRouter>
-        <Tuits tuits={MOCKED_TUITS}/>
-      </HashRouter>);
-  const linkElement = screen.getByText(/ellen\'s tuit/i);
-  expect(linkElement).toBeInTheDocument();
-
+  // TODO: implement this
 });
 
 test('tuit list renders async', async () => {
-    const tuits = await findAllTuits();
-    render(
-        <HashRouter>
-            <Tuits tuits={tuits}/>
-        </HashRouter>);
-    const linkElement = screen.getByText(/larry tuit/i);
-    expect(linkElement).toBeInTheDocument();
+  // TODO: implement this
 })
 
 test('tuit list renders mocked', async () => {
-    const mock = jest.spyOn(axios, 'get');
-  mock.mockImplementation(() =>
-      Promise.resolve({ data: {tuits: MOCKED_TUITS} }));
-  const response = await findAllTuits();
-  const tuits = response.tuits;
-  render(
-      <HashRouter>
-        <Tuits tuits={tuits}/>
-      </HashRouter>);
-  const tuit = screen.getByText(/ellen\'s tuit/i);
-  expect(tuit).toBeInTheDocument();
-  mock.mockRestore();
+  // TODO: implement this
 });
--- src/tests/user-list.test.js
+++ src/tests/user-list.test.js
@@ -2,10 +2,10 @@ import {UserList} from "../components/profile/user-list";
 import {screen, render} from "@testing-library/react";
 import {HashRouter} from "react-router-dom";
 import {findAllUsers} from "../services/users-service";
-
 import axios from "axios";
+import {createUser} from "./services";
 
-//jest.mock('axios');
+jest.mock('axios');
 
 const MOCKED_USERS = [
   {username: 'ellen_ripley', password: 'lv426', email: 'repley@weyland.com', _id: "123"},
@@ -24,18 +24,17 @@ test('user list renders static user array', () => {
 test('user list renders async', async () => {
   const users = await findAllUsers();
   render(
-      <HashRouter>
-        <UserList users={users}/>
-      </HashRouter>);
-  const linkElement = screen.getByText(/moe/i);
+    <HashRouter>
+      <UserList users={users}/>
+    </HashRouter>);
+  const linkElement = screen.getByText(/alice/i);
   expect(linkElement).toBeInTheDocument();
-
 })
 
 test('user list renders mocked', async () => {
-  const mock = jest.spyOn(axios, 'get');
-  mock.mockImplementation(() =>
+  axios.get.mockImplementation(() =>
     Promise.resolve({ data: {users: MOCKED_USERS} }));
+
   const response = await findAllUsers();
   const users = response.users;
 
@@ -46,5 +45,4 @@ test('user list renders mocked', async () => {
 
   const user = screen.getByText(/ellen_ripley/i);
   expect(user).toBeInTheDocument();
-  mock.mockRestore();
 });
